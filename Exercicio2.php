9f3eba718ce1381fc05e310572537b483a7d9c31
Lucas Vieira <vieira07lucas@gmail.com>
2023-03-01 12:22:29 -0300

    Exercícios resolvidos

---
+++ Exercicio2.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Exercício 2</title>
+</head>
+
+<body>
+    <?php 
+
+    $frase = "TESTE";
+    echo $frase . "<br>";
+    echo strrev($frase);
+
+    ?>
+</body>
+
+</html>
